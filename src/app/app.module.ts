071bb1c72acd7494ca85a397fac339bf338c8897
Roderick Mastrolinardo <roderick0302@hotmail.com>
2020-02-08 22:49:34 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarComponent } from './car-list/car/car.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RentCarComponent } from './rent-car/rent-car.component';
+import { HistoryComponent } from './history/history.component';
+import {Routes, RouterModule, Router} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: CarListComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'rent', component: RentCarComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'signIn', component: LoginComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    RentCarComponent,
+    HistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rent-car/rent-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rent-car',
+  templateUrl: './rent-car.component.html',
+  styleUrls: ['./rent-car.component.scss']
+})
+export class RentCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
